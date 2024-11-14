769c4bed7fcb198f39e5cc54913cb5eb168eb38d
HanishDhanwalkar <hanishpd1729@gmail.com>
2024-11-14 13:00:03 +0530

    Final Streamlit local app

--- app.py
+++ app.py
@@ -13,32 +13,23 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
 
     while run:
-        # #########
-        ret, frame = cap.read()
-        
-        
-        if not ret:
-            st.write("ERROR: Could not read frame")
-            break
-        else:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            FRAME_WINDOW.image(frame)
-        # ###########
         
         # ret, frame = cap.read()
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # FRAME_WINDOW.image(frame)
+
+        ret, frame = cap.read()
         
-        # if not ret:
-        #     break
+        if not ret:
+            break
         
-        # results = yolo.predict(frame)
-        # frame, annotated_frame = yolo.annotate(frame, results)    
+        results = yolo.predict(frame)
+        frame, annotated_frame = yolo.annotate(frame, results)    
         
-        # annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
-        # FRAME_WINDOW.image(annotated_frame)
+        annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(annotated_frame)
         
     else:
         cap.release()
         cv2.destroyAllWindows()
         st.write('Stopped')
-
-    
